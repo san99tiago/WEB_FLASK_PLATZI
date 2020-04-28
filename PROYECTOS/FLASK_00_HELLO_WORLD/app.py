f532398a506c6506059f098fd0f1fab77772cf0d
san99tiago <san99tiago@gmail.com>
2020-04-28 04:00:37 -0500

    primer hello world desde flask

--- PROYECTOS/FLASK_00_HELLO_WORLD/app.py
+++ PROYECTOS/FLASK_00_HELLO_WORLD/app.py
@@ -2,6 +2,9 @@
 #PRIMERA APP PARA MOSTRAR A TRAVES DE FLASK UN MENSAJE
 #Santiago Garcia Arango
 #--------------------------------------------------------
+#Nota: Ademas de este archivo, ver "correr.sh" para correr con respectivos comandos...
+#...que debemos indicar en terminal para correr servidor adecuadamente
+#(ademas de indicarlos, permite automatizar proceso tedioso repetitivo)
 
 from flask import Flask
 
@@ -11,4 +14,4 @@ app = Flask( __name__ )
 #Con este decorador, logramos acceder a ruta "/" (osea la ruta por defecto)
 @app.route("/")
 def hola():
-    return( "MI PRIMERA APP EN FLASK" )
+    return( "MI PRIMERA APP EN FLASK!!" )
