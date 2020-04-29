b2d56bdfd4f585bf74bf1017b22b93a6c7cff267
san99tiago <san99tiago@gmail.com>
2020-04-29 01:54:36 -0500

    main app flask

---
+++ PROYECTOS/FLASK_07_STATIC/app.py
@@ -0,0 +1,52 @@
+#----------------------------------------------------------------------------------
+#APP PARA EXTENDER UTILIDAD DE FLASK Y AGREGAR ARCHIVOS ESTATICOS (COMO IMAGENES)
+#Santiago Garcia Arango
+#----------------------------------------------------------------------------------
+#Nota: Ademas de este archivo, ver "correr.sh" para correr con respectivos comandos...
+#...en terminal, para correr servidor flask adecuadamente.
+#(ademas de indicarlos, permite automatizar proceso tedioso repetitivo en terminal)
+
+from flask import Flask , request , make_response , redirect , render_template
+
+#Indicamos que app va a tener como parametro el nomre de este archivo
+app = Flask( __name__ )
+
+
+#Vector que pasaremos hacia flask (para mostrar ciclos a traves de Jinja2 en HTML)
+vector_cool = [1,1,2,3,5,8,13,21,34,55,89,144]
+
+
+#Con este decorador, logramos acceder a ruta "/" (osea la ruta por defecto)
+@app.route("/")
+def index():
+    #Accedemos a la IP actual del usuario (utilizando request)
+    #Cuando se desarrolle app con servidor global, se podria acceder a esta IP de cada usuario
+    IP_usuario = request.remote_addr
+
+    #Ahora creamos un "response" que nos redirecciona a "/hello"
+    respuesta = make_response( redirect("/hello") )
+    
+    #Ademas, creamos COOKIE para el IP_usuario
+    respuesta.set_cookie( "IP_usuario" , IP_usuario )
+
+    #Ejecutamos respuesta (que tiene tanto redirect, como cookie del IP)
+    return( respuesta )
+
+
+#Ruta que va a re-dirigir index, y recibira la respuesta, junto con la cookie
+@app.route("/hello")
+def hello():
+    #Creamos IP_usuario a traves de request hacia las cookies y la "llave" que identificaba el IP_usuario
+    IP_usuario = request.cookies.get( "IP_usuario" )
+
+    #Como empezamos a tener muchos parametros, es buena practica enviarlos a render_template...
+    #...en un diccinarios de "contexto" que tenga todos estos parametros respectivos
+    context = {
+        "IP_usuario" : IP_usuario,
+        "vector_cool" : vector_cool
+    }
+
+    #Indicamos un render_template que permite indicar el HTML al que queremos ir, ademas de la variable IP_usuario
+    #NOTA: al indicar **context , nos aprovechamos de python para expandir diccionario "context" y enviar cada...
+    #...uno de sus keys y keywords hacia flask (equivalente a enviar todos uno a uno por aparte)
+    return( render_template("hello.html", **context) )
