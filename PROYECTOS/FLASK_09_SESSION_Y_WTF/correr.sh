7a82a82b853e0170a5f413ffdb6bd8e3a255044e
san99tiago <san99tiago@gmail.com>
2020-04-30 02:21:16 -0500

    cambios terminal para correr bien en GNU BASH

--- PROYECTOS/FLASK_09_SESSION_Y_WTF/correr.sh
+++ PROYECTOS/FLASK_09_SESSION_Y_WTF/correr.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 #ARCHIVO TIPO BASH PARA "AUTOMATIZAR" COMANDOS TERMINAL
+#OJO:formato de estos archivos es similar al de GNU (CAMBIA SINTAXIS)
+#por lo tanto, si se fueran a correr en windows terminal, ver equivalencia
 #Al correrlo, correra estos comandos en terminal por nosotros
 
 #Activar virtual env en caso de que NO este activo
-../../env/Scripts/activate
+#(en windows cmd:  >>../../env/Scripts/activate)
+source ../../env/Scripts/activate
 
 #Definir archivo flask por defecto para correr app (nombre archivo python)
-set FLASK_APP=app.py
+#(en windows cmd:  >>set FLASK_APP=app.py)
+export FLASK_APP=app.py
 
 #Activar modo debug para verificar errores y tener mas control
+#(en windows cmd:  >>set FLASK_DEBUG=1)
 export FLASK_DEBUG=1
 
 #Permitie activar el entorno "development" en app
-set FLASK_ENV=development
+#(en windows cmd:  >>set FLASK_ENV=development)
+export FLASK_ENV=development
 
-#Correr "servidor" de flask
+#Correr "servidor" de flask (igual en windows)
 flask run
